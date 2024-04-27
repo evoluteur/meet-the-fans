ccdf6fc87cf7387989e921cb534b1099c8144483
Olivier Giulieri <ogiulieri@gmail.com>
2024-04-26 23:52:09 -0700

    Changed faded color.

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const config = {
     fork: "#1966AC",
     both: "#4A96C9",
   },
-  colorFaded: "#7ca6dd", //"#5594F4", // '#e1e1e1', '#eeeeee',
+  colorFaded: "#3f3f3f", //"#5594F4", // '#e1e1e1', '#eeeeee',
   circleBorder: "white",
   // misc.
   maxTopics: 5,
